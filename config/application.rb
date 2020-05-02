b1a3fbeab5f43d687bd0ebb441200a71a6718e34
Kasper Langer <kasper@chainalysis.com>
2020-05-02 17:18:49 +0200

    Disable assets and helpers

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Reviewminator
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
